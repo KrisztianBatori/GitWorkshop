216a700de18cfd85f944495da689730333257950
KrisztianBatori <batori.krisztian97@gmail.com>
2019-04-01 12:08:50 +0200

    Cheer one param and main test

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,14 +1,14 @@
 public class Cheer {
 
     public static void main(String[] args) {
-        cheer();
+        cheer(5);
         Greet.greet();
         Count.count();
         Future.future();
     }
 
-    public static void cheer() {
-        System.out.println("Pindúr Pandúrok!");
+    public static void cheer(int param) {
+        System.out.println("Pindúr Pandúrok!" + "My sweety param: " + param);
     }
 
 }
