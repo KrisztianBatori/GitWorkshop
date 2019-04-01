2d251b5f577bb333053092a6c1d15e9ada5d1787
Majoros Aniko <aniko.majoros87@gmail.com>
2019-04-01 14:23:13 +0200

    a6

--- src/Future.java
+++ src/Future.java
@@ -1,7 +1,7 @@
 public class Future {
     
     public static void future(int valamiParameter){
-        System.out.println("Vízöntő vagyok most is" + valamiParameter);
+        System.out.println("Your future looks like" + valamiParameter);
         System.out.println("My chinese horoscope!");
     }
 }
