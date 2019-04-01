242c95aa084c5e6de1d10aa9cc86cfee4fd9e9a9
Varga Balint <vargabalint@gmail.com>
2019-04-01 14:24:28 +0200

    b6

--- src/Greet.java
+++ src/Greet.java
@@ -1,6 +1,6 @@
 public class Greet {
 
-    public static void greet(){
-        System.out.println("Greetings!");
+    public static void greet(String parameter){
+        System.out.println("Greetings for the"+ parameter);
     }
 }
