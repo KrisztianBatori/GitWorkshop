46ff671c7e2e816c35c6383adf4d68f9f7061fe8
benciitoo <bence.banszegi@gmail.com>
2019-04-01 14:43:47 +0200

    count javitas

--- src/Count.java
+++ src/Count.java
@@ -2,7 +2,7 @@ public class Count {
 
     public static void count() {
         System.out.println("Counting the countless parameter: " + 7);
-
+    }
 
     public static void count(int age, int name) {
         System.out.println(name+":"+age+"years old");
