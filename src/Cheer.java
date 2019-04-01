7085b280db8b54fb6960dbfe82fb8a0979715757
Varga Balint <vargabalint@gmail.com>
2019-04-01 12:36:41 +0200

    b3

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,6 +1,6 @@
 public class Cheer {
 
     public static void cheer(int param) {
-        System.out.println("Pindúr Pandúrok!" + "My sweeety param: " + param);
+        System.out.println("java");
     }
 }
