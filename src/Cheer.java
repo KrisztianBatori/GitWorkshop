de3438de2a7ec2f20c39a8062680dd91fe00b405
KrisztianBatori <batori.krisztian97@gmail.com>
2019-04-01 12:17:08 +0200

    Main separated from cheer class

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,14 +1,6 @@
 public class Cheer {
 
-    public static void main(String[] args) {
-        cheer(5);
-        Greet.greet("Anikó");
-        Count.count(33);
-        Future.future(99);
-    }
-
     public static void cheer(int param) {
         System.out.println("Pindúr Pandúrok!" + "My sweety param: " + param);
     }
-
 }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Cheer.cheer(5);
+        Greet.greet("Anikó");
+        Count.count(33);
+        Future.future(99);
+    }
+}
